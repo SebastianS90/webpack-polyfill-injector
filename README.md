ecea044fb1c79a1601b19f01834bc20f1327ac23
Sebastian Schweizer <sebastian@schweizer.tel>
2017-02-21 22:01:17 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,137 @@
+[![npm][npm]][npm-url]
+[![deps][deps]][deps-url]
+[![test][test]][test-url]
+
+# Webpack Polyfill Injector Plugin
+This plugin uses polyfills from [polyfill-service](https://github.com/Financial-Times/polyfill-service) and loads them if needed.
+
+Instead of punishing all users with an additional HTTP request or increased script size, only browsers that do not implement all required features natively will load the polyfills from a separate file.
+
+Webpack's internal chunk loading feature relies on `Promise` and therefore cannot be used to load a `Promise` polyfill. That is why this plugin implements its own loading logics.
+
+
+## Install
+```bash
+yarn add webpack-polyfill-injector --dev
+```
+
+or
+
+```bash
+npm install webpack-polyfill-injector --save-dev
+```
+
+
+## Usage
+```javascript
+const PolyfillInjectorPlugin = require('webpack-polyfill-injector');
+
+module.exports = {
+  entry: {...},
+  output: {...},
+  module: {...},
+  plugins: [
+    new PolyfillInjectorPlugin([
+      'Promise',
+      'Array.prototype.find',
+    ])
+  ]
+};
+```
+
+You can use any [polyfill from `polyfill-service`](https://github.com/Financial-Times/polyfill-service/tree/master/polyfills).
+
+
+## Advanced Options
+### Specify the polyfills filename
+By default, we use the filename from webpack's `output.filename` setting with `[name]` replaced by `polyfills`. For example, if `output.filename` is `js/[name].[hash].js` then the polyfills will be written to `js/polyfills.0123456789abcdef.js`.
+
+You can override the filename as follows:
+
+```javascript
+new PolyfillInjectorPlugin({
+  polyfills: [
+    'Promise',
+    'Array.prototype.find',
+  ],
+  filename: 'some/path/polyfills.[hash:6].js',
+})
+```
+
+### Use `polyfill.io` CDN
+If you prefer to load polyfills directly from the `polyfill.io` CDN instead of putting a file into your bundle, then you can pass the following options:
+
+```javascript
+new PolyfillInjectorPlugin({
+  polyfills: [
+    'Promise',
+    'Array.prototype.find',
+  ],
+  service: true,
+})
+```
+
+If any specified polyfill is required, then the browser will load
+`https://cdn.polyfill.io/v2/polyfill.min.js?features=Promise,Array.prototype.find`.
+
+Note that `polyfill.io` only sends those polyfills that are required by the requesting browser.
+
+### Use your own `polyfill-service` installation
+If want to use the benefits of only sending required polyfills but do not like to load scripts from the `polyfill.io` CDN, then you can use your own installation of `polyfill-service`:
+
+```javascript
+new PolyfillInjectorPlugin({
+  polyfills: [
+    'Promise',
+    'Array.prototype.find',
+  ],
+  service: 'https://static.mydomain.example/js/polyfills.js',
+})
+```
+
+Like above, `?features=Promise,Array.prototype.find` will be appended to the service URL.
+
+### Specify any existing polyfill script
+Do you already have a file with all required polyfills? Then you can use:
+
+```javascript
+new PolyfillInjectorPlugin({
+  polyfills: [
+    'Promise',
+    'Array.prototype.find',
+  ],
+  service: 'https://static.mydomain.example/js/polyfills.js',
+})
+```
+
+## Technical Details
+The plugin wraps all entry chunks as follows:
+
+```javascript
+(function(main) {
+  if (any specified polyfill is required) {
+    var js = document.createElement('script');
+    js.src = 'url to the polyfills script';
+    js.onload = main;
+    js.onerror = function() {
+      console.error('Could not load polyfills script!');
+      main();
+    };
+    document.head.appendChild(js);
+  } else {
+    main();
+  }
+}) (function() {
+// your entry chunk goes here...
+});
+```
+
+
+[npm]: https://img.shields.io/npm/v/webpack-polyfill-injector.svg
+[npm-url]: https://npmjs.com/package/webpack-polyfill-injector
+
+[deps]: https://david-dm.org/SebastianS90/webpack-polyfill-injector.svg
+[deps-url]: https://david-dm.org/SebastianS90/webpack-polyfill-injector
+
+[test]: https://secure.travis-ci.org/SebastianS90/webpack-polyfill-injector.svg
+[test-url]: http://travis-ci.org/SebastianS90/webpack-polyfill-injector
---
+++ src/index.js
@@ -0,0 +1,152 @@
+const
+    OriginalSource = require('webpack-sources').OriginalSource,
+    ReplaceSource = require('webpack-sources').ReplaceSource,
+    ConcatSource = require('webpack-sources').ConcatSource,
+    PrefixSource = require('webpack-sources').PrefixSource,
+    CachedSource = require('webpack-sources').CachedSource,
+    loaderUtils = require('loader-utils'),
+    _ = require('lodash'),
+    fs = require('fs');
+
+
+function PolyfillInjectorPlugin(arg) {
+    // Allow to pass an array as argument
+    const config = Array.isArray(arg)
+        ? {polyfills: arg}
+        : arg;
+
+    // Ensure that we have at least one polyfill
+    if (!config || !config.polyfills || !config.polyfills.length) {
+        throw new Error('No polyfills specified!');
+    }
+    this.polyfills = config.polyfills.map(polyfill => polyfill.replace(/\./g, '/'));
+
+    if (config.service) {
+        // Use a hosted polyfill-service
+        this.service = true;
+        this.src = typeof config.service === 'string'
+            ? config.service
+            : 'https://cdn.polyfill.io/v2/polyfill.min.js';
+    } else if (config.src) {
+        // Use a static URL
+        this.src = config.src;
+    } else {
+        // Bundle the polyfills and serve them from outputPath
+        this.filename = config.filename;
+    }
+}
+
+function loadFileAsSource(file) {
+    const resolvedFile = require.resolve(file);
+    return new OriginalSource(
+        fs.readFileSync(resolvedFile, {encoding: 'utf8'}).trimRight(),
+        resolvedFile
+    );
+}
+
+function loadFileAsString(file) {
+    const resolvedFile = require.resolve(file);
+    return fs.readFileSync(resolvedFile, {encoding: 'utf8'}).trim();
+}
+
+function buildInjector(polyfills, src) {
+    const injectorRaw = loadFileAsSource('./injector.js');
+    const injector = new ReplaceSource(injectorRaw);
+
+    // Replace __TEST__
+    const testPosition = injectorRaw.source().indexOf('__TEST__');
+    injector.replace(testPosition, testPosition + 7,
+        polyfills.map(polyfill =>
+            `/* ${polyfill.replace(/\//g, '.')} */!(` +
+            loadFileAsString(`polyfill-service/polyfills/${polyfill}/detect.js`) +
+            ')'
+        ).join('\n    || ')
+    );
+
+    // Replace __SRC__
+    const srcPosition = injectorRaw.source().indexOf('__SRC__');
+    injector.replace(srcPosition, srcPosition + 6, JSON.stringify(src));
+
+    return injector;
+}
+
+PolyfillInjectorPlugin.prototype.apply = function apply(compiler) {
+    compiler.plugin('compilation', (compilation) => {
+        let src = '';
+        if (this.service) {
+            src = this.src + '?features=' + this.polyfills
+                .map(polyfill => encodeURIComponent(polyfill.replace(/\//g, '.')))
+                .join(',');
+        } else if (this.src) {
+            src = this.src;
+        } else {
+            // Construct the bundled polyfills file
+            const content = new CachedSource(
+                _.flatMap(this.polyfills, polyfill => [
+                    `/* ${polyfill.replace(/\//g, '.')} */\nif (!(`,
+                    loadFileAsSource(`polyfill-service/polyfills/${polyfill}/detect.js`),
+                    ')) {\n',
+                    new PrefixSource(
+                        '\t',
+                        loadFileAsSource(`polyfill-service/polyfills/${polyfill}/polyfill.js`)
+                    ),
+                    '\n}\n\n',
+                ]).reduce((concatSource, entry) => {
+                    concatSource.add(entry);
+                    return concatSource;
+                }, new ConcatSource())
+            );
+            content.__PolyfillInjectorPlugin = true;
+
+            // Determine the output file
+            const file = loaderUtils.interpolateName(
+                {resourcePath: './polyfills.js'},
+                this.filename || compilation.options.output.filename,
+                {content: content.source()}
+            );
+            src = compilation.options.output.publicPath + file;
+
+            // Add the file as asset for all entry chunks
+            compilation.plugin('additional-chunk-assets', (chunks) => {
+                compilation.assets[file] = content;
+                chunks.forEach((chunk) => {
+                    if (chunk.hasEntryModule()) {
+                        chunk.files.push(file);
+                    }
+                });
+            });
+        }
+
+        // Inject the script to load our polyfill file
+        const injector = buildInjector(this.polyfills, src);
+        compilation.plugin('optimize-chunk-assets', (chunks, callback) => {
+            chunks.forEach((chunk) => {
+                if (!chunk.hasEntryModule()) {
+                    return;
+                }
+                chunk.files.forEach((file) => {
+                    if (!file.endsWith('.js') ||
+                        compilation.assets[file].__PolyfillInjectorPlugin
+                    ) {
+                        return;
+                    }
+
+                    // (function(main) {
+                    //   load polyfills if needed, then execute main()
+                    // }) (function() { normal application code goes here... });
+                    const source = new ConcatSource(
+                        injector,
+                        '(function() {\n',
+                        compilation.assets[file],
+                        '\n});'
+                    );
+                    source.__PolyfillInjectorPlugin = true;
+                    compilation.assets[file] = source;
+                });
+            });
+            callback();
+        });
+    });
+};
+
+module.exports = PolyfillInjectorPlugin;
